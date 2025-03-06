742ae229a09745396f07ef83fde753c58ca4cbe5
dharma23459 <dharma23459@gmail.com>
2025-03-06 10:31:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-south-1"
+}
+
+resource "aws_instance" "dharma" {
+    ami = "ami-0ddfba243cbee3768"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "Dharma"
+    }
+}
